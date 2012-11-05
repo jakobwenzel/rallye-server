bf1dcede24022f46886382107a7b70558a0cd5da
Felix Hübner <mail@fhuebner.de>
2012-11-05 19:38:18 +0100

    added server command "getChatEntries". for more info visit the wiki. fixed an exception while add a chat entry without a message but a picture. add is not marked as working any more. Problem here is, that if an error occure while processing the new chat entry, the database is not set back to a status before add the chat entry.

--- src/de/stadtrallye/control/ClientListener.java
+++ src/de/stadtrallye/control/ClientListener.java
@@ -3,6 +3,7 @@ package de.stadtrallye.control;
 import java.util.HashSet;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -12,6 +13,8 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
+
 import de.stadtrallye.model.ChatHandler;
 import de.stadtrallye.model.DataHandler;
 import de.stadtrallye.model.MapHandler;
@@ -27,144 +30,172 @@ import de.stadtrallye.resource.exceptions.SQLHandlerException;
 public class ClientListener {
 
 	DataHandler data = new DataHandler();
-	
-	//==================================================================//
-	//    Map Commands
-	//==================================================================//
-	
-	
-	/*@Path("map/getNodes")
-	@GET
-	@Produces(MediaType.APPLICATION_JSON) 
-	public JSONArray getNodes() {
-		return MapHandler.getAllNodes(this.data);
-	}*/
-	
+
+	// ==================================================================//
+	// Map Commands
+	// ==================================================================//
+
+	/*
+	 * @Path("map/getNodes")
+	 * 
+	 * @GET
+	 * 
+	 * @Produces(MediaType.APPLICATION_JSON) public JSONArray getNodes() {
+	 * return MapHandler.getAllNodes(this.data); }
+	 */
+
 	@Path("map/getAllNodes")
 	@GET
-	@Produces(MediaType.APPLICATION_JSON) 
+	@Produces(MediaType.APPLICATION_JSON)
 	public JSONArray getAllNodes() {
-		//return Response.status(201).entity(MapHandler.getAllNodes(this.data).toString()).build();
+		// return
+		// Response.status(201).entity(MapHandler.getAllNodes(this.data).toString()).build();
 		return MapHandler.getAllNodes(this.data);
 	}
+
+	// ==================================================================//
+	// Chat Commands
+	// ==================================================================//
+
+	@Path("chat/getChatEntries/{userID}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public JSONArray getChatEntries(@PathParam("userID") String user) throws SQLHandlerException, JSONException {
+		return ChatHandler.getChatEntries(this.data,user,0);
+	}
 	
-	//==================================================================//
-	//    Chat Commands
-	//==================================================================//
 	
+	@Path("chat/getChatEntries/{userID}/{timestamp}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public JSONArray getChatEntries(@PathParam("userID") String user, @PathParam("timestamp") int timestamp) throws SQLHandlerException, JSONException {
+		return ChatHandler.getChatEntries(this.data,user,timestamp);
+	}
+	
+	@Path("chat/setNewChatEntry/{userID}")
+	@POST
+	@Consumes(MediaType.APPLICATION_OCTET_STREAM)
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response setNewChatEntry(byte[] payload, @PathParam("userID") String user) {
+		return this.setNewChatEntryInternal(payload, user, null);
+	}
+
 	@Path("chat/setNewChatEntry/{userID}/{message}")
 	@POST
 	@Consumes(MediaType.APPLICATION_OCTET_STREAM)
 	@Produces(MediaType.TEXT_PLAIN)
-	public Response setNewChatEntry(byte[] payload,@PathParam("userID") String user, @PathParam("message") String message ) {
-		// some debug print
-		System.out.println("User: "+user+" Message: '"+message+"' Payload: "+(payload.length!=0? "available":"empty"));
-		
-		
+	public Response setNewChatEntry(byte[] payload, @PathParam("userID") String user, @PathParam("message") String message) {
+		return this.setNewChatEntryInternal(payload, user, message);
+	}
+
+	/**
+	 * method to add a chat entry to database ( and a copy to file)
+	 * @param payload the picture if available
+	 * @param user userID GCM id !!!
+	 * @param message message if avaliable
+	 * @return a response with the http return code (and a error message)
+	 * @author Felix HŸbner
+	 */
+	private Response setNewChatEntryInternal(byte[] payload, String user, String message) {
 		HashSet<Integer> c;
+		
+		// some debug print
+		System.out.println("User: " + user + " Message: '" + message + "' Payload: " + (payload.length != 0 ? "available" : "empty"));
+		
 		try {
 			// process ChatEntry
-			c = ChatHandler.setNewChatEntry(this.data,payload, user, message);
+			c = ChatHandler.setNewChatEntry(this.data, payload, user, message);
 			// update devices in chatrooms
 			if (!c.isEmpty()) {
 				this.data.updateDevices(c);
 			}
 			return Response.status(201).build();
-			
+
 		} catch (SQLHandlerException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			return Response.status(400).entity(e.getErrorCode()+" : "+e.getMessage()).build();
+			return Response.status(500).entity(e.getMessage()).build();
 		}
-		
 	}
-	
-	//==================================================================//
-	//    Other Commands
-	//==================================================================//
-	
+
+	// ==================================================================//
+	// Other Commands
+	// ==================================================================//
+
 	@Path("getStatus")
 	@GET
-	@Produces(MediaType.APPLICATION_JSON) 
+	@Produces(MediaType.APPLICATION_JSON)
 	public JSONArray getStatus() {
-		//return Response.status(201).entity(OtherHandler.getStatus(this.data).toString()).build();
+		// return
+		// Response.status(201).entity(OtherHandler.getStatus(this.data).toString()).build();
 		return OtherHandler.getStatus(this.data);
 	}
-	
-	
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	// ==================================================================//
+	// Helper Methods
+	// ==================================================================//
+
 	/**
-	 *  this method will respond to a chat event. 
+	 * this method will respond to a chat event.
+	 * 
 	 * @param type
 	 * @return
 	 * @author Felix HŸbner
 	 */
-	/*@Path("chat/{command}")
-	@GET
-	@Produces({"text/plain", "application/json"})
-	public Response getChatEvent(@PathParam("command") String command) {
-		command = command.toLowerCase();
-		
-		//if (false) {
-			//return this.data.map.getAllNodes();
-		//} else {
-		
-		StringBuilder str = new StringBuilder();
-		str.append("Respond to CHAT-Event");
-		return Response.status(201).entity(str.toString()).build();
-		//}
-	}
-	*/
+	/*
+	 * @Path("chat/{command}")
+	 * 
+	 * @GET
+	 * 
+	 * @Produces({"text/plain", "application/json"}) public Response
+	 * getChatEvent(@PathParam("command") String command) { command =
+	 * command.toLowerCase();
+	 * 
+	 * //if (false) { //return this.data.map.getAllNodes(); //} else {
+	 * 
+	 * StringBuilder str = new StringBuilder();
+	 * str.append("Respond to CHAT-Event"); return
+	 * Response.status(201).entity(str.toString()).build(); //} }
+	 */
 	/**
-	 *  this method will respond to a map event. 
+	 * this method will respond to a map event.
+	 * 
 	 * @param type
 	 * @return
 	 * @author Felix HŸbner
 	 */
-	/*@Path("map/{command}")
-	@GET
-	//@Produces("application/json")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response getMapEvent(@PathParam("command") String command) {
-		//error handling (the boolean must be an AND to be sure to stop checking if data does not exist)
-		if (this.data == null) {
-			throw new WebApplicationException(500);
-			//return Response.status(500).entity("data does not exist!!!").build();
-		}
-		command = command.toLowerCase();
-		
-		
-		if (command.equals("getallnodes")) {
-			return Response.status(201).entity(MapHandler.getAllNodes(this.data).toString()).build();
-		} else {
-			//StringBuilder str = new StringBuilder();
-			//str.append("Respond to MAP-Event on host: http://"+this.data.getUri()+":"+this.data.getPort());
-			throw new WebApplicationException(400);
-			//return Response.status(201).entity(str.toString()).build();
-		}
-	}
-	*/
-	
-	
+	/*
+	 * @Path("map/{command}")
+	 * 
+	 * @GET //@Produces("application/json")
+	 * 
+	 * @Produces(MediaType.APPLICATION_JSON) public Response
+	 * getMapEvent(@PathParam("command") String command) { //error handling (the
+	 * boolean must be an AND to be sure to stop checking if data does not
+	 * exist) if (this.data == null) { throw new WebApplicationException(500);
+	 * //return Response.status(500).entity("data does not exist!!!").build(); }
+	 * command = command.toLowerCase();
+	 * 
+	 * 
+	 * if (command.equals("getallnodes")) { return
+	 * Response.status(201).entity(MapHandler
+	 * .getAllNodes(this.data).toString()).build(); } else { //StringBuilder str
+	 * = new StringBuilder();
+	 * //str.append("Respond to MAP-Event on host: http://"
+	 * +this.data.getUri()+":"+this.data.getPort()); throw new
+	 * WebApplicationException(400); //return
+	 * Response.status(201).entity(str.toString()).build(); } }
+	 */
+
 	// only for debug
-	/*@Path("{x: .*}")
-	@GET
-	public Response getOtherEvent() {
-		return Response.status(404).entity("Unknown Event!!").build();
-		//throw new WebApplicationException(404);
-	}*/
-	
+	/*
+	 * @Path("{x: .*}")
+	 * 
+	 * @GET public Response getOtherEvent() { return
+	 * Response.status(404).entity("Unknown Event!!").build(); //throw new
+	 * WebApplicationException(404); }
+	 */
+
 	// @Consumes(MediaType.APPLICATION_JSON)
 	// @Produces(MediaType.APPLICATION_JSON)
 }
--- src/de/stadtrallye/model/ChatHandler.java
+++ src/de/stadtrallye/model/ChatHandler.java
@@ -6,6 +6,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Savepoint;
 import java.util.HashSet;
+
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
 import de.stadtrallye.resource.exceptions.SQLHandlerException;
 
 /**
@@ -34,7 +39,7 @@ public class ChatHandler {
 
 			// create savePoint for a rollback if an error occurs
 			sPoint = con.setSavepoint();
-			if (!message.isEmpty()) {
+			if (message != null && !message.isEmpty()) {
 				// create prepared statement for insert message into ry_messages
 				stmt = con.prepareStatement("INSERT INTO ry_messages (message) VALUES (?)");
 				stmt.setString(1, message);
@@ -54,18 +59,22 @@ public class ChatHandler {
 				stmt.close();
 			}
 
-			if (!message.isEmpty() || pic.length != 0) {
+			if ((message != null && !message.isEmpty()) || pic.length != 0) {
 				// get all chatroom where we have to post this entry
 				ResultSet rs = con.createStatement().executeQuery(
 						"SELECT cg.chatroomID FROM ry_chatrooms_groups as cg, ry_clients as c WHERE c.gcmRegID = '"+userID+"' AND c.groupID = cg.groupID");
-				Boolean empty = rs.first();
-				
-				while (empty) {
+				Boolean available = rs.first();
+				if (!available) {
+					con.rollback(sPoint);
+					con.releaseSavepoint(sPoint);
+					throw new SQLHandlerException("Add entry to database failed!: User unknown!");
+				}
+				while (available) {
 
 					// create new chat entry into ry_chats
 					stmt = con.prepareStatement("INSERT INTO ry_chats (timestamp,groupID,messageID,pictureID,chatroomID) VALUES (" + "now(),"
 							+ "(SELECT groupID FROM ry_clients WHERE gcmRegID = '"+userID+"'),"
-							+ (!message.isEmpty() ? "(SELECT MAX(messageID) FROM ry_messages)," : "NULL,")
+							+ ((message != null && !message.isEmpty()) ? "(SELECT MAX(messageID) FROM ry_messages)," : "NULL,")
 							+ (pic.length != 0 ? "(SELECT MAX(pictureID) FROM ry_pictures)," : "NULL,") + rs.getInt(1) +")");
 					stmt.execute();
 					// debug
@@ -75,10 +84,14 @@ public class ChatHandler {
 					chatrooms.add(rs.getInt(1));
 					
 					// move result element-curser 1 element further
-					empty = rs.next();
+					available = rs.next();
 
 				}
 				rs.close();
+			} else {
+				con.rollback(sPoint);
+				con.releaseSavepoint(sPoint);
+				throw new SQLHandlerException("Add entry to database failed!: Unknown Error!");
 			}
 
 			// apply changes in database
@@ -100,5 +113,51 @@ public class ChatHandler {
 			throw new SQLHandlerException(e1);
 		}
 		return chatrooms;
+	}
+	
+	
+	public static JSONArray getChatEntries(DataHandler dh, String user, int timestamp) throws SQLHandlerException, JSONException {
+		Connection con = dh.getSqlCon();
+		JSONArray lst = new JSONArray();
+		JSONObject o = null;
+		
+		try {
+			PreparedStatement stmt = con.prepareStatement("SELECT distinct UNIX_TIMESTAMP(c.timestamp) as timestamp,c.groupID,c.pictureID,c.messageID,(SELECT message FROM ry_messages WHERE messageID = c.messageID) as message " 
+					+ "FROM ry_chats as c, ry_messages as m, ry_chatrooms_groups as cg, ry_clients as cl "
+					+ "WHERE cl.groupID = cg.groupID AND cg.chatroomID = c.chatroomID AND c.timestamp >= FROM_UNIXTIME(?) AND cl.gcmRegID = ? ORDER BY c.timestamp ASC");
+			stmt.setInt(1, timestamp);
+			stmt.setString(2, user);
+			ResultSet rs = stmt.executeQuery();
+			
+			Boolean available = rs.first();
+			while (available) {
+				o = new JSONObject();
+				o.put("timestamp", rs.getInt("timestamp"));
+				o.put("groupID",rs.getInt("groupID"));
+				if (rs.getInt("messageID") != 0) {
+				o.put("message", rs.getString("message"));
+				}
+				if (rs.getInt("pictureID") != 0) {
+					o.put("picture", rs.getInt("pictureID"));
+				}
+				//add object to array
+				lst.put(o);
+				
+				//DEBUG
+				System.out.println(o.toString());
+				
+				// jump to next return row
+				available = rs.next();
+			}
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new SQLHandlerException(e.getMessage());
+		}
+		
+		return lst;
+		
 	}
 }
