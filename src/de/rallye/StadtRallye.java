03b03d68d5361a695ce3fc35c83c34c6ae875ffe
Felix Hübner <mail@fhuebner.de>
2012-11-28 21:47:20 +0100

    added a Console on port 10100 to be able to stop the server. the console is only available from localhost, all other connections will be closed.

--- src/de/rallye/StadtRallye.java
+++ src/de/rallye/StadtRallye.java
@@ -5,6 +5,7 @@ import java.beans.PropertyVetoException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import de.rallye.control.GameConsole;
 import de.rallye.control.GameHandler;
 
 /**
@@ -24,29 +25,6 @@ public class StadtRallye {
 		//create and init new GameHandler		
 		GameHandler game = new GameHandler((args.length > 0 ? args[0]: null));
 		
-		//temporary to hold the server open
-		while(true) {
-			try {
-				Thread.sleep(5000);
-			} catch (InterruptedException e) {
-				logger.throwing(e);
-			}
-		}
-		
-		
-		/*logger.info("Hit enter to stop server...");
-		try {
-			System.in.read();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-		
-		game.stopServer();
-		//game.start();
-		logger.exit();*/
 	}
 
 }
---
+++ src/de/rallye/control/GameConsole.java
@@ -0,0 +1,108 @@
+package de.rallye.control;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import de.rallye.StadtRallye;
+import de.rallye.resource.DataHandler;
+
+/**
+ * @author Felix HŸbner
+ * @version 1.0
+ * 
+ */
+public class GameConsole {
+	Logger logger = LogManager.getLogger(GameConsole.class.getName());
+	private DataHandler data = null;
+
+	private ServerSocket socket = null;
+
+	/**
+	 *  create a tcp-console on localhost.
+	 */
+	public GameConsole(DataHandler data) {
+		logger.entry();
+		this.data = data;
+
+		try {
+			socket = new ServerSocket(10100);
+		} catch (IOException e) {
+			logger.catching(e);
+		}
+		logger.exit();
+	}
+
+	/**
+	 * wait for next connection
+	 * @return return true if the server should be closed, otherwise false
+	 * @author Felix HŸbner
+	 */
+	public boolean accept() {
+		logger.entry();
+		Socket client = null;
+		BufferedReader input = null;
+		PrintWriter output = null;
+		String inputLine;
+
+		try {
+			client = socket.accept();
+			
+			logger.trace("New client to address: "+client.getLocalAddress().getHostAddress()+"");
+			if (!client.getLocalAddress().getHostAddress().equals("127.0.0.1")) {
+				client.close();
+				logger.warn("client not allowed! close connection. Bye Bye!");
+				return false;
+			}
+			
+			output = new PrintWriter(client.getOutputStream(), true);
+			input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+			output.println("RallyeServ Telnet Server. Send ? for help.");
+			logger.info("Client logged into Server Console from: "+client.getInetAddress()+":"+client.getPort());
+
+			while ((inputLine = input.readLine()) != null) {
+				if (inputLine.equals("?")) {
+					output.println("Available Commands: quit,stop.");
+				} else if (inputLine.equals("quit")) {
+					logger.info("Client Disconnected.");
+					output.println("Bye.");
+					break;
+				} else if (inputLine.equals("stop")) {
+					logger.info("Stopping Server. Bye.");
+					output.println("Stopping Server. Bye.");
+					return logger.exit(true);
+				} else {
+					output.println("Unknown Command. Send ? for help.");
+				}
+			}
+			input.close();
+			output.close();
+			client.close();
+		} catch (IOException e) {
+			logger.catching(e);
+		}
+
+		return logger.exit(false);
+	}
+	
+	/**
+	 * try to close the Game Console Socket
+	 * 
+	 * @author Felix HŸbner
+	 */
+	public void close() {
+		try {
+			socket.close();
+		} catch (IOException e) {
+			logger.catching(e);
+		}
+	}
+
+}
