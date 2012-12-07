8d44bb2355216c67e501168a019d98352fa5c44d
Felix Hübner <mail@fhuebner.de>
2012-12-07 21:32:23 +0100

    added the methods / rest-commands getAllEdges und getConfig, moved /status to /system/status.

--- src/de/rallye/rest/ClientListener.java
+++ src/de/rallye/rest/ClientListener.java
@@ -37,14 +37,20 @@ public class ClientListener {
 	// Map Commands
 	// ==================================================================//
 
-	@Path("map/get/nodes")
+	@Path("map/nodes")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getAllNodes() {
 		logger.entry();
-		// return
-		// Response.status(201).entity(MapHandler.getAllNodes(this.data).toString()).build();
 		return logger.exit(this.data.getAllNodes());
+	}
+	
+	@Path("map/edges")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getAllEdges() {
+		logger.entry();
+		return logger.exit(this.data.getAllEdges());
 	}
 
 	// ==================================================================//
@@ -119,10 +125,10 @@ public class ClientListener {
 	}
 
 	// ==================================================================//
-	// Other Commands
+	// System Commands
 	// ==================================================================//
 
-	@Path("status")
+	@Path("system/status")
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
@@ -131,110 +137,16 @@ public class ClientListener {
 		return logger.exit(this.data.getStatus(o));
 	}
 
-	/*@Path("postPic")
-	@POST
+	@Path("system/config")
+	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public JSONObject test(byte[] pic) {
+	public Response getConfig() {
 		logger.entry();
-		JSONObject o = new JSONObject();
-
-		try {
-			o.put("pic1", ClientListener.hexEncode(pic));
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		return logger.exit(o);
-	}*/
+		return logger.exit(this.data.getConfig());
+	}
+	
 
 	// ==================================================================//
 	// Helper Methods
 	// ==================================================================//
-
-	private static Appendable hexEncode(byte buf[], Appendable sb) {
-		// final Formatter formatter = new Formatter(sb);
-		for (int i = 0; i < buf.length; i++) {
-			int low = buf[i] & 0xF;
-			int high = (buf[i] >> 8) & 0xF;
-			try {
-				sb.append(Character.forDigit(high, 16));
-				sb.append(Character.forDigit(low, 16));
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		return sb;
-	}
-
-	private static String hexEncode(byte buf[]) {
-		String s = hexEncode(buf, new StringBuilder()).toString();
-		// System.out.println(s);
-		return s;
-	}
-
-	/**
-	 * this method will respond to a chat event.
-	 * 
-	 * @param type
-	 * @return
-	 * @author Felix HŸbner
-	 */
-	/*
-	 * @Path("chat/{command}")
-	 * 
-	 * @GET
-	 * 
-	 * @Produces({"text/plain", "application/json"}) public Response
-	 * getChatEvent(@PathParam("command") String command) { command =
-	 * command.toLowerCase();
-	 * 
-	 * //if (false) { //return this.data.map.getAllNodes(); //} else {
-	 * 
-	 * StringBuilder str = new StringBuilder();
-	 * str.append("Respond to CHAT-Event"); return
-	 * Response.status(201).entity(str.toString()).build(); //} }
-	 */
-	/**
-	 * this method will respond to a map event.
-	 * 
-	 * @param type
-	 * @return
-	 * @author Felix HŸbner
-	 */
-	/*
-	 * @Path("map/{command}")
-	 * 
-	 * @GET //@Produces("application/json")
-	 * 
-	 * @Produces(MediaType.APPLICATION_JSON) public Response
-	 * getMapEvent(@PathParam("command") String command) { //error handling (the
-	 * boolean must be an AND to be sure to stop checking if data does not
-	 * exist) if (this.data == null) { throw new WebApplicationException(500);
-	 * //return Response.status(500).entity("data does not exist!!!").build(); }
-	 * command = command.toLowerCase();
-	 * 
-	 * 
-	 * if (command.equals("getallnodes")) { return
-	 * Response.status(201).entity(MapHandler
-	 * .getAllNodes(this.data).toString()).build(); } else { //StringBuilder str
-	 * = new StringBuilder();
-	 * //str.append("Respond to MAP-Event on host: http://"
-	 * +this.data.getUri()+":"+this.data.getPort()); throw new
-	 * WebApplicationException(400); //return
-	 * Response.status(201).entity(str.toString()).build(); } }
-	 */
-
-	// only for debug
-	/*
-	 * @Path("{x: .*}")
-	 * 
-	 * @GET public Response getOtherEvent() { return
-	 * Response.status(404).entity("Unknown Event!!").build(); //throw new
-	 * WebApplicationException(404); }
-	 */
-
-	// @Consumes(MediaType.APPLICATION_JSON)
-	// @Produces(MediaType.APPLICATION_JSON)
 }
