8a0a6561c8f98bcc824609f7ec30453db5e42f0b
Ramon Wirsch <ramon.wirsch@stud.tu-darmstadt.de>
2013-07-03 03:21:52 +0200

    * WebClient works deployed in .jar

--- src/de/rallye/api/Client.java
+++ src/de/rallye/api/Client.java
@@ -1,8 +1,8 @@
 package de.rallye.api;
 
 import java.io.File;
+import java.io.InputStream;
 import java.net.URISyntaxException;
-import java.net.URL;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -19,11 +19,12 @@ import com.sun.jersey.multipart.file.DefaultMediaTypePredictor;
 
 import de.rallye.RallyeResources;
 import de.rallye.RallyeServer;
+import de.rallye.StadtRallye;
 import de.rallye.exceptions.WebAppExcept;
 
 @Path("client")
 public class Client {
-	private static final String RESOURCE_PATH = "de/rallye/webclient/";
+	private static final String RESOURCE_PATH = "webclient/";
 
 	private Logger logger =  LogManager.getLogger(Client.class);
 
@@ -34,26 +35,20 @@ public class Client {
 	public Response index(@PathParam("path") String path, @Context SecurityContext sec) {
 		
 		if (path.contains("/"))
-			throw new WebAppExcept(404, "Not found.");
+			throw new WebAppExcept(404, "Path invalid");
 		
 		logger.debug("Trying to load "+RESOURCE_PATH+path);
 		
-		URL url = this.getClass().getClassLoader().getResource(RESOURCE_PATH+path);
+		InputStream stream = StadtRallye.class.getResourceAsStream(RESOURCE_PATH+path);
 		
-		if (url==null) {
+		if (stream==null) {
 
 			throw new WebAppExcept(404, "Not found.");
 		}
 		
-		try {
-			File f = new File(url.toURI());
-			MediaType m = DefaultMediaTypePredictor.CommonMediaTypes.getMediaTypeFromFile(f);
-			return Response.ok(f,m).build();
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-			throw new WebAppExcept(404, "Not found.");
-		}
-		
+		MediaType m = DefaultMediaTypePredictor.CommonMediaTypes.getMediaTypeFromFileName(path);
+		return Response.ok(stream,m).build();
+	
 	}
 	
 	@GET
