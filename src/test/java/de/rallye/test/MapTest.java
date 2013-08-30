97a44621b69b0afcca0c7e11f37efda17c5a3452
Jakob Wenzel <jakobwenzel92@gmail.com>
2013-08-30 23:22:10 +0200

    Moved test cases from TestOne to GroupsTest, added Server startup to every Test Class

--- src/test/java/de/rallye/test/MapTest.java
+++ src/test/java/de/rallye/test/MapTest.java
@@ -2,13 +2,21 @@ package de.rallye.test;
 
 import static org.junit.Assert.fail;
 
+import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
+
+import de.rallye.test.helper.StartTestServer;
 
 
 @Ignore
 public class MapTest {
 
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		StartTestServer.getServer(); //Start a server
+	}
+	
 	@Test
 	public void testGetNodes() {
 		fail("Not yet implemented");
--- src/test/java/de/rallye/test/TasksTest.java
+++ src/test/java/de/rallye/test/TasksTest.java
@@ -2,12 +2,20 @@ package de.rallye.test;
 
 import static org.junit.Assert.fail;
 
+import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
+
+import de.rallye.test.helper.StartTestServer;
 
 @Ignore
 public class TasksTest {
 
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		StartTestServer.getServer(); //Start a server
+	}
+	
 	@Test
 	public void testGetTasks() {
 		fail("Not yet implemented");
