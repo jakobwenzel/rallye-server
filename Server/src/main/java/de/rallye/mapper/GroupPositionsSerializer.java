377613438d9184a363652a9ab0304f5992fe2ed1
Ramon Wirsch <ramon.wirsch@stud.tu-darmstadt.de>
2014-09-11 23:10:36 +0200

    * upgrade libs, gradle

--- Server/src/main/java/de/rallye/mapper/GroupPositionsSerializer.java
+++ Server/src/main/java/de/rallye/mapper/GroupPositionsSerializer.java
@@ -19,16 +19,14 @@
 
 package de.rallye.mapper;
 
-import java.io.IOException;
-import java.util.Map;
-
-import org.codehaus.jackson.JsonGenerator;
-import org.codehaus.jackson.JsonProcessingException;
-import org.codehaus.jackson.map.JsonSerializer;
-import org.codehaus.jackson.map.SerializerProvider;
-
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
 import de.rallye.model.structures.Group;
 import de.rallye.model.structures.Node;
+
+import java.io.IOException;
+import java.util.Map;
 
 public class GroupPositionsSerializer extends JsonSerializer<Map<Integer,Node>> {
 
